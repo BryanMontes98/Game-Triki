2473574f6476eee8c64c49bff002c13bdc4567f6
Bryan Montes <montesceballosb@gmail.com>
2026-08-29 19:17:45 -0500

    An extension was added to connect ngrok

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['73da-186-81-100-81.ngrok-free.app'],
+  },
 })
